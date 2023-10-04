9b5c4ed4af14643b08f5ea0ee75ed86045391a6c
ThanhF <trthanh179@gmail.com>
2023-10-04 07:22:38 +0700

    UPdate more more more

---
+++ script.js
@@ -0,0 +1,79 @@
+function leapYear(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+function validDay(day) {
+    if (day == null || day == "") {
+        return "Day empty";
+    }
+    if (isNaN(day)) {
+        return "Day is not a number";
+    }
+    if (day <= 0 || day >= 32) {
+        return "Day out of range";
+    }
+}
+
+function validMonth(month) {
+    if (month == null || month == "") {
+        return "month empty";
+    }
+    if (isNaN(month)) {
+        return "month is not a number";
+    }
+    if (month <= 0 || month >= 13) {
+        return "month out of range";
+    }
+}
+
+function validYear(year) {
+    if (year == null || year == "") {
+        return "year empty";
+    }
+    if (isNaN(year)) {
+        return "year is not a number";
+    }
+    if (year <= 0 || year >= 9999) {
+        return "year out of range";
+    }
+}
+
+function validDayInMonth(day, month) {
+    switch (month) {
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            if (day == 31) {
+                return "Day not 31"
+            }
+            break;
+        case 2:
+            if (day >= 29) {
+                return "February not more than 29 days"
+            }
+            else if (!leapYear(year) && day == 29) {
+                return "February in Leap year not more than 28 days"
+            }
+            break;
+    }
+}
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let errorArray = []
+    let day = document.querySelector('#day').value
+    let month = document.querySelector('#month').value
+    let year = document.querySelector('#year').value
+    errorArray.push(validDay(day))
+    errorArray.push(validMonth(month))
+    errorArray.push(validYear(year))
+    errorArray.push(validDayInMonth(day, month))
+    errorArray = errorArray.filter(a => a)
+    if (errorArray.length == 0) {
+        document.querySelector('#Messagelabel').innerHTML = "Oke"
+    }
+    else {
+        document.querySelector('#Messagelabel').innerHTML = errorArray
+    }
+})
